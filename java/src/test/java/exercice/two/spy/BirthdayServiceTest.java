a82c9237641fb7e4d507a63bfa1772ff15c0eb15
Ayoub Nasri <ayoub1.nasri@sofrecom.com>
2021-11-30 16:03:49 +0100

    Working...

--- java/src/test/java/exercice/two/spy/BirthdayServiceTest.java
+++ java/src/test/java/exercice/two/spy/BirthdayServiceTest.java
@@ -1,19 +1,24 @@
 package exercice.two.spy;
 
-import static org.junit.Assert.*;
-
 import org.junit.Test;
 import org.mockito.Mockito;
 
+import clients.ClientRepository;
+import exercice.one.stub.BirthdayService;
 import notification.Mailer;
 
 public class BirthdayServiceTest {
+	
+	ClientRepository clientRepository = Mockito.mock(ClientRepository.class);
+	BirthdayService service = new BirthdayService(clientRepository);
 	
 	
 	@Test
 	public void testName() throws Exception {
 		
 		Mailer mailer = Mockito.mock(Mailer.class);
+		
+		service.greeting("Fred");
 		
 		Mockito.verify(mailer).send("Fred");
 		
