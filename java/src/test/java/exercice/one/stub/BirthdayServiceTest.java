88297c065b3d8503602d6a54b08e4184d57818fb
Ayoub Nasri <ayoub1.nasri@sofrecom.com>
2021-11-30 15:41:32 +0100

    Working...

--- java/src/test/java/exercice/one/stub/BirthdayServiceTest.java
+++ java/src/test/java/exercice/one/stub/BirthdayServiceTest.java
@@ -1,12 +1,35 @@
 package exercice.one.stub;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.*;
 
 import java.util.List;
 
+import org.assertj.core.api.Assertions;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.internal.configuration.injection.scanner.MockScanner;
+
+import clients.ClientRepository;
 
 public class BirthdayServiceTest {
+	
+	
+	
+	@Test
+	public void testName() throws Exception {
+		
+		ClientRepository clientRepository = Mockito.mock(ClientRepository.class);
+		BirthdayService service = new BirthdayService(clientRepository);
+		
+		//Test
+		
+		String result = service.greeting("fred");
+		
+		Assertions.assertThat(result).isEqualTo("sdfhbj");
+		
+	}
   
   
 
