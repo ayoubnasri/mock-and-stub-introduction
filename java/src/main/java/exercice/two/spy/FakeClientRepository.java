5add5d63a876d8507ee38c0d0b77a8dcc97ebecb
Ayoub Nasri <ayoub1.nasri@sofrecom.com>
2021-11-30 17:03:36 +0100

    Working...

--- java/src/main/java/exercice/two/spy/FakeClientRepository.java
+++ java/src/main/java/exercice/two/spy/FakeClientRepository.java
@@ -5,10 +5,10 @@ import clients.ClientRepository;
 public class FakeClientRepository extends ClientRepository{
 	
 	public String birthdayFor;
+	
 	@Override
 	public void setBirthdayFor(String name) {
 		this.birthdayFor = name ; 
-		
 	}
 
 }
