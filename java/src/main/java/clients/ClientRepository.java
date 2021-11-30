4f7693e1a8a3c1084dc6254be9e2b075f9b6343a
johan.martinsson <johan.martinsson@dexyos.fr>
2021-11-30 16:54:24 +0100

    Working...

--- java/src/main/java/clients/ClientRepository.java
+++ java/src/main/java/clients/ClientRepository.java
@@ -2,6 +2,10 @@ package clients;
 
 public class ClientRepository {
 
+    public void setBirthdayFor(String name) {
+        
+    }
+
   public boolean birthdayIsTodayFor(String name) {
     // TODO Auto-generated method stub
     return false;
