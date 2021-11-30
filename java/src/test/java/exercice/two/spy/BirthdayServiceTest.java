8cb41787c5889232fe347b9f37931732edb5dc0f
Ayoub Nasri <ayoub1.nasri@sofrecom.com>
2021-11-30 16:01:45 +0100

    Working...

--- java/src/test/java/exercice/two/spy/BirthdayServiceTest.java
+++ java/src/test/java/exercice/two/spy/BirthdayServiceTest.java
@@ -1,6 +1,24 @@
 package exercice.two.spy;
 
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import notification.Mailer;
+
 public class BirthdayServiceTest {
+	
+	
+	@Test
+	public void testName() throws Exception {
+		
+		Mailer mailer = Mockito.mock(Mailer.class);
+		
+		Mockito.verify(mailer).send("Fred");
+		
+	}
+	
   
   
 
