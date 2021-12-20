50572a64b81d5d6b8c2f42ad3f94dc833f5c285a
Ayoub Nasri <ayoub1.nasri@sofrecom.com>
2021-12-20 12:24:00 +0100

    Working...

--- java/src/main/java/roulette/RouletteService.java
+++ java/src/main/java/roulette/RouletteService.java
@@ -3,7 +3,6 @@ package roulette;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.util.Random;
-import java.util.concurrent.TimeUnit;
 
 public class RouletteService {	
 	
@@ -11,15 +10,11 @@ public class RouletteService {
 	
 	public int getRandomNumber(int timeout) throws NoSuchAlgorithmException, InterruptedException {
 
-		getTimeOut(timeout);
+		timeOutAdapter.getTimeOut(timeout);
 		Random rand = SecureRandom.getInstanceStrong(); 
 
 		return rand.ints(1, 0, 36).findFirst().getAsInt();
 
-	}
-
-	private void getTimeOut(int timeout) throws InterruptedException {
-		TimeUnit.SECONDS.sleep(timeout);
 	}
 
 }
--- java/src/main/java/roulette/TimeOutAdapter.java
+++ java/src/main/java/roulette/TimeOutAdapter.java
@@ -1,5 +1,11 @@
 package roulette;
 
+import java.util.concurrent.TimeUnit;
+
 public class TimeOutAdapter {
+
+	void getTimeOut(int timeout) throws InterruptedException {
+		TimeUnit.SECONDS.sleep(timeout);
+	}
 
 }
