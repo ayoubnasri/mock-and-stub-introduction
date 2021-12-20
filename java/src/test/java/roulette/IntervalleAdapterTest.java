90e401c8e429d792e3725885d71bc52576193a69
rania <rania.yahmadi@sofrecom.com>
2021-12-20 15:24:02 +0100

    Working...

--- java/src/test/java/roulette/IntervalleAdapterTest.java
+++ java/src/test/java/roulette/IntervalleAdapterTest.java
@@ -1,5 +1,19 @@
 package roulette;
 
-public class IntervalleAdapterTest {
+import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.Test;
+
+public class IntervalleAdapterTest {
+	@Test
+	public void check_random_number_ok() throws Exception {
+		
+		//test
+		IntervalleAdapter intervalleAdapter 
+		int rslt = rouletteService.getRandomNumber();
+		int rslt2 = rouletteService.getRandomNumber();
+		//assert
+		assertThat(rslt).isNotEqualTo(rslt2);
+		
+	}
 }
