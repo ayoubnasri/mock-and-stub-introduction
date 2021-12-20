89b1ee8d227a57f87e3b00abf223a818b171174b
rania <rania.yahmadi@sofrecom.com>
2021-12-20 15:09:38 +0100

    Working...

--- java/src/main/java/roulette/IntervalleAdapter.java
+++ java/src/main/java/roulette/IntervalleAdapter.java
@@ -1,5 +1,14 @@
 package roulette;
 
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.util.Random;
+
 public class IntervalleAdapter {
+	
+	private int getRandomValue() throws NoSuchAlgorithmException {
+		Random rand = SecureRandom.getInstanceStrong(); 
+		return rand.ints(1, 0, 36).findFirst().getAsInt();
+	}
 
 }
