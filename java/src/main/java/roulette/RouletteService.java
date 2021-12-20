dc5066d1f059c6341e79ee97317942214c9904c4
Ayoub Nasri <ayoub1.nasri@sofrecom.com>
2021-12-20 11:48:59 +0100

    Working...

--- java/src/main/java/roulette/RouletteService.java
+++ java/src/main/java/roulette/RouletteService.java
@@ -3,13 +3,15 @@ package roulette;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 public class RouletteService {
 
 	public int getRandomNumber() throws NoSuchAlgorithmException, InterruptedException {
 
 
-		wait(20*1000);
+		TimeUnit.SECONDS.sleep(20);
+
 		Random rand = SecureRandom.getInstanceStrong(); 
 
 		return rand.ints(1, 0, 36).findFirst().getAsInt();
