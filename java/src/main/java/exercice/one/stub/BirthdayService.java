0aefab58351d2faab24904614c115a15e2365169
rania <rania.yahmadi@sofrecom.com>
2021-12-20 10:45:20 +0100

    Working...

--- java/src/main/java/exercice/one/stub/BirthdayService.java
+++ java/src/main/java/exercice/one/stub/BirthdayService.java
@@ -6,7 +6,7 @@ public class BirthdayService {
 
 	private ClientRepository repository;
 
-	public BirthdayService(ClientRepository repository) {
+	 BirthdayService(ClientRepository repository) {
 		this.repository = repository;
 
 	}
