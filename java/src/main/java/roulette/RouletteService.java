84e7652383ca3aafada538840ae297fe56c10932
Ayoub Nasri <ayoub1.nasri@sofrecom.com>
2021-12-20 12:26:08 +0100

    Working...

--- java/src/main/java/roulette/RouletteService.java
+++ java/src/main/java/roulette/RouletteService.java
@@ -6,7 +6,7 @@ import java.util.Random;
 
 public class RouletteService {	
 	
-	TimeOutAdapter timeOutAdapter; 
+	TimeOutAdapter timeOutAdapter = new TimeOutAdapter(20); 
 	
 	public int getRandomNumber() throws NoSuchAlgorithmException, InterruptedException {
 
