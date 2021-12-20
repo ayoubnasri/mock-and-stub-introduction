9be0b37540e0c11fb42e1b27ab8f1a6aba976862
Ayoub Nasri <ayoub1.nasri@sofrecom.com>
2021-12-20 11:24:33 +0100

    Working...

--- java/src/test/java/roulette/RouletteServiceTest.java
+++ java/src/test/java/roulette/RouletteServiceTest.java
@@ -1,22 +1,32 @@
 package roulette;
 
+import static org.junit.Assert.*;
+
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
 
 public class RouletteServiceTest {
 	
-	
+	RouletteService rouletteService = new RouletteService();
 
 	@Test
 	public void check_random_number_ok() throws Exception {
 		//setUp
-		RouletteService rouletteService = new RouletteService();
+		
 		
 		//test
 		int rslt = rouletteService.getRandomNumber();
 		
 		//assert
 		Assertions.assertThat(rslt).isBetween(0, 36);
+	}
+	
+	@Test
+	public void testName() throws Exception {
+		
+		
+		//test
+		
 	}
 	
 
