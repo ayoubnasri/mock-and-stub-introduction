52e7230ac77dce4c801d8e0f1ddb5853eb91c179
rania <rania.yahmadi@sofrecom.com>
2021-12-20 11:34:44 +0100

    Working...

--- java/src/main/java/roulette/RouletteService.java
+++ java/src/main/java/roulette/RouletteService.java
@@ -5,20 +5,12 @@ import java.util.Random;
 public class RouletteService {
 
 	public int getRandomNumber() {
-<<<<<<< HEAD
-		
-		Random r = new Random();
-=======
+
 		Random r = new Random();
 
-<<<<<<< HEAD
+
 		return r.ints(1, 0, 36).findFirst().getAsInt();
-=======
->>>>>>> b56023a8c24fd7b4582e069aefcc56cc11f5356b
-		int randomNumber = r.ints(1, 0, 36).findFirst().getAsInt();
 
-		return randomNumber;
->>>>>>> 61e272385ad9b8182602cecd5e1ed66e0d800d43
 	}
 
 }
