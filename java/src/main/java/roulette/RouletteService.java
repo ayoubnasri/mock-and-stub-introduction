0826c058b03be5c2b54504bac0a5ce4bfd73d993
rania <rania.yahmadi@sofrecom.com>
2021-12-20 11:32:51 +0100

    Working...

--- java/src/main/java/roulette/RouletteService.java
+++ java/src/main/java/roulette/RouletteService.java
@@ -1,13 +1,13 @@
 package roulette;
 
+import java.util.Random;
+
 public class RouletteService {
 
 	public int getRandomNumber() {
 		Random r = new Random();
 
-		int randomNumber = r.ints(1, 0, 36).findFirst().getAsInt();
-
-		return randomNumber;
+		return r.ints(1, 0, 36).findFirst().getAsInt();
 	}
 
 }
