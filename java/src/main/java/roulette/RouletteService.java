e7f4c5ff6ffa2ceb54abbd179a174c67ff4ecaba
Ayoub Nasri <ayoub1.nasri@sofrecom.com>
2021-12-20 11:32:43 +0100

    Working...

--- java/src/main/java/roulette/RouletteService.java
+++ java/src/main/java/roulette/RouletteService.java
@@ -1,9 +1,15 @@
 package roulette;
 
+import java.util.Random;
+
 public class RouletteService {
 
 	public int getRandomNumber() {
-		return (int) Math.random();
+		
+		Random r = new Random();
+		int randomNumber = r.ints(1, 0, 36).findFirst().getAsInt();
+
+		return randomNumber;
 	}
 
 }
