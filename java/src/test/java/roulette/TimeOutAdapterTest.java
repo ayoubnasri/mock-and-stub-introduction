601db09e672c1b2ad21361f156b75bc4fc9fae02
rania <rania.yahmadi@sofrecom.com>
2021-12-20 14:47:10 +0100

    Working...

--- java/src/test/java/roulette/TimeOutAdapterTest.java
+++ java/src/test/java/roulette/TimeOutAdapterTest.java
@@ -1,5 +1,29 @@
+import java.util.concurrent.TimeUnit;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
 public class TimeOutAdapterTest  {
-	
+	@Test
+	public void check_random_number_reponse_time_ok() throws Exception {
+		
+		//setUp
+		
+		//Mockito.doReturn(20).when(timeOutAdapter).getTimeOut();
+		timeOutAdapter = new TimeOutAdapter(20);
+		//ReflectionTestUtils.setField(timeOutAdapter, "timeOutAdapter", new TimeOutAdapter(20));
+
+		
+		//test
+		long currentTimeBefore = System.currentTimeMillis();
+		int rslt = rouletteService.getRandomNumber();
+		long currentTimeAfter = System.currentTimeMillis();
+		
+
+		//assert
+		long diffTime = TimeUnit.MILLISECONDS.toSeconds(currentTimeAfter - currentTimeBefore);
+		Assertions.assertThat(diffTime).isEqualTo(20);
+		
 	
 	
 	
