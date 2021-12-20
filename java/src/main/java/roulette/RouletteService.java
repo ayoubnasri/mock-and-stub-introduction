b0537f5c2079094a9cd9d364a910dd3a5d31d7f8
rania <rania.yahmadi@sofrecom.com>
2021-12-20 15:07:55 +0100

    Working...

--- java/src/main/java/roulette/RouletteService.java
+++ java/src/main/java/roulette/RouletteService.java
@@ -8,29 +8,6 @@ public class RouletteService {
 	
 	private TimeOutAdapter timeOutAdapter; 
 	
-	public RouletteService(int min, int max) {
-		this.min = min;
-		this.max = max; 
-	}
-	private int min;
-	private int max;
-	
-	public int getMin() {
-		return min;
-	}
-
-	public void setMin(int min) {
-		this.min = min;
-	}
-
-	public int getMax() {
-		return max;
-	}
-
-	public void setMax(int max) {
-		this.max = max;
-	}
-
 	public TimeOutAdapter getTimeOutAdapter() {
 		return timeOutAdapter;
 	}
