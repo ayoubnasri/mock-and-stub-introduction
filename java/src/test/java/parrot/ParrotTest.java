1ab7eae80fc884a1ac45a5119b45ff6ab884ece2
habiba <habiba.kessraoui@sofrecom.com>
2021-12-21 10:37:31 +0100

    Working...

--- java/src/test/java/parrot/ParrotTest.java
+++ java/src/test/java/parrot/ParrotTest.java
@@ -7,28 +7,18 @@ import static org.junit.Assert.assertEquals;
 public class ParrotTest {
 
 	@Test
-	public void getSpeedOfEuropeanParrot_Polymorphisme() throws Exception {
+	public void getSpeedOfEuropeanParrot() throws Exception {
 		EuropeanParrot europeanParrot = new EuropeanParrot(0, false);
         assertEquals(12.0, europeanParrot.getSpeed(), 0.0);
 	}
 	
 	@Test
-    public void getSpeedOfAfricanParrot_With_One_Coconut_Polymorphisme() {
+    public void getSpeedOfAfricanParrot_With_One_Coconut() {
         AfricanParrot africanParrot = new AfricanParrot(1, 0, false);
         assertEquals(3.0, africanParrot.getSpeed(), 0.0);
     }
 	
-    @Test
-    public void getSpeedOfEuropeanParrot() {
-        Parrot parrot = new Parrot(ParrotTypeEnum.EUROPEAN, 0, 0, false);
-        assertEquals(12.0, parrot.getSpeed(), 0.0);
-    }
 
-    @Test
-    public void getSpeedOfAfricanParrot_With_One_Coconut() {
-        Parrot parrot = new Parrot(ParrotTypeEnum.AFRICAN, 1, 0, false);
-        assertEquals(3.0, parrot.getSpeed(), 0.0);
-    }
 
     @Test
     public void getSpeedOfAfricanParrot_With_Two_Coconuts() {
