f602ecaace720cc63ba48aa835b4888a68bbcada
Ayoub Nasri <ayoub1.nasri@sofrecom.com>
2021-12-21 14:52:55 +0100

    Working...

--- java/src/main/java/duplicationkata/Lesson1Straight.java
+++ java/src/main/java/duplicationkata/Lesson1Straight.java
@@ -9,11 +9,12 @@ public class Lesson1Straight extends Song
 	
 	List<String> refrains = Arrays.asList("But the cat came back","The very next day","Oh the cat came back", "We thought he was a goner","But the cat came back, he just wouldn't go away");
 	
+	List<String> singPart = Arrays.asList("We gave the cat to a little kid","We sent the cat out on a boat");
   public void singCatSong()
   {
-    singSong("We gave the cat to a little kid");
-    
-    singSong("We sent the cat out on a boat");
+	  for(String s:singPart) {
+		  singSong("We gave the cat to a little kid");
+	  }
   }
 
 	private void singSong(String s) {
