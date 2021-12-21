f6c8c10ff65699872bce1c4eb01ad03ed5713045
habiba <habiba.kessraoui@sofrecom.com>
2021-12-21 11:11:36 +0100

    Working...

--- java/src/main/java/parrot/Parrot.java
+++ java/src/main/java/parrot/Parrot.java
@@ -2,13 +2,12 @@ package parrot;
 
 public class Parrot {
 
-    private ParrotTypeEnum type;
-
     public Parrot() {
     }
 
     public double getSpeed() {
-    	throw new RuntimeException("Should be unreachable");
+		return 0;
+    	//throw new RuntimeException("Should be unreachable");
     }
 
     protected double getLoadFactor() {
