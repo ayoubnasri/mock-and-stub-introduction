7eb862422c4247b20ee83a216fd374bf697904b4
habiba <habiba.kessraoui@sofrecom.com>
2021-12-21 11:07:02 +0100

    Working...

--- java/src/test/java/parrot/ParrotTest.java
+++ java/src/test/java/parrot/ParrotTest.java
@@ -8,38 +8,38 @@ public class ParrotTest {
 
 	@Test
 	public void getSpeedOfEuropeanParrot() throws Exception {
-		EuropeanParrot europeanParrot = new EuropeanParrot(0, false);
+		EuropeanParrot europeanParrot = new EuropeanParrot();
         assertEquals(12.0, europeanParrot.getSpeed(), 0.0);
 	}
 	
 	@Test
     public void getSpeedOfAfricanParrot_With_One_Coconut() {
-        AfricanParrot africanParrot = new AfricanParrot(1, 0, false);
+        AfricanParrot africanParrot = new AfricanParrot(1);
         assertEquals(3.0, africanParrot.getSpeed(), 0.0);
     }
 
     @Test
     public void getSpeedOfAfricanParrot_With_Two_Coconuts() {
-        AfricanParrot africanParrot = new AfricanParrot(2, 0, false);
+        AfricanParrot africanParrot = new AfricanParrot(2);
         assertEquals(0.0, africanParrot.getSpeed(), 0.0);
     }
 
     @Test
     public void getSpeedOfAfricanParrot_With_No_Coconuts() {
-        AfricanParrot africanParrot = new AfricanParrot(0, 0, false);
+        AfricanParrot africanParrot = new AfricanParrot(0);
         assertEquals(12.0, africanParrot.getSpeed(), 0.0);
     }
 
     @Test
     public void getSpeedNorwegianBlueParrot_nailed() {
     	NorwegianParrot norwegianParrot = new NorwegianParrot(1.5, true);
-        assertEquals(0.0, parrot.getSpeed(), 0.0);
+        assertEquals(0.0, norwegianParrot.getSpeed(), 0.0);
     }
 
     @Test
     public void getSpeedNorwegianBlueParrot_not_nailed() {
-        Parrot parrot = new Parrot(ParrotTypeEnum.NORWEGIAN_BLUE, 0, 1.5, false);
-        assertEquals(18.0, parrot.getSpeed(), 0.0);
+    	NorwegianParrot norwegianParrot = new NorwegianParrot(1.5, false);
+        assertEquals(18.0, norwegianParrot.getSpeed(), 0.0);
     }
 
     @Test
