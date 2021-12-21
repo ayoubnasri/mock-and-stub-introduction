a74d877506bec02b44df41e2a0e4dc142447af86
Ayoub Nasri <ayoub1.nasri@sofrecom.com>
2021-12-21 15:15:28 +0100

    Working...

--- java/src/main/java/duplicationkata/Lesson3HigherOrderFunctions.java
+++ java/src/main/java/duplicationkata/Lesson3HigherOrderFunctions.java
@@ -5,22 +5,32 @@ public class Lesson3HigherOrderFunctions extends Song
   public void singCheers()
   {
     int number = 2;
+    
     sing(number + "! ");
+    
     number = number + 2;
     sing(number + "! ");
+    
     number = number + 2;
     sing(number + "! ");
+    
     number = number + 2;
     sing(number + "! ");
+    
     sing("Who do we appreciate?");
+    
     number = 17;
     sing(number + "! ");
+    
     number = getNextPrime(number);
     sing(number + "! ");
+    
     number = getNextPrime(number);
     sing(number + "! ");
+    
     number = getNextPrime(number);
     sing(number + "! ");
+    
     sing("These are the primes, that we find fine!");
   }
   private int getNextPrime(int number)
