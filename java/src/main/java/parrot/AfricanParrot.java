f9631eaeacbdaa1e0395caa1435686d43eb8e234
habiba <habiba.kessraoui@sofrecom.com>
2021-12-21 10:27:58 +0100

    Working...

--- java/src/main/java/parrot/AfricanParrot.java
+++ java/src/main/java/parrot/AfricanParrot.java
@@ -3,10 +3,13 @@ package parrot;
 
 public class AfricanParrot extends Parrot{
 
+    private int numberOfCoconuts;
+
 	public AfricanParrot(ParrotTypeEnum type, int numberOfCoconuts, double voltage, boolean isNailed) {
 		super(type, numberOfCoconuts, voltage, isNailed);
 	}
 
+	@Override
 	public double getSpeed() {
         return Math.max(0, getBaseSpeed() - getLoadFactor() * numberOfCoconuts);
 	}
