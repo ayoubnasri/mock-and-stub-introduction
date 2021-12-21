818f1c01aa7bf6f0c668c8d39056e0c1d9f9e7c0
Ayoub Nasri <ayoub1.nasri@sofrecom.com>
2021-12-21 14:59:54 +0100

    Working...

--- java/src/main/java/duplicationkata/Lesson2Variable.java
+++ java/src/main/java/duplicationkata/Lesson2Variable.java
@@ -4,13 +4,18 @@ public class Lesson2Variable extends Song
 {
   public void singBottlesOfBeer()
   {
-    sing("100 bottles of beer on the wall");
-    sing("100 bottles of beer");
-    sing("Take one down, pass it around");
-    sing("99 bottles of beer on the wall");
+    singRefrain(100);
+    
     sing("99 bottles of beer on the wall");
     sing("99 bottles of beer");
     sing("Take one down, pass it around");
     sing("98 bottles of beer on the wall");
   }
+
+private void singRefrain(int i1) {
+	sing("100 bottles of beer on the wall");
+    sing("100 bottles of beer");
+    sing("Take one down, pass it around");  
+    sing("99 bottles of beer on the wall");
+}
 }
