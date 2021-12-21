3fac4f5ff084067dda7922af75cc622fd4b48fb4
Ayoub Nasri <ayoub1.nasri@sofrecom.com>
2021-12-21 16:45:21 +0100

    Working...

--- java/src/main/java/duplicationkata/Lesson31.java
+++ java/src/main/java/duplicationkata/Lesson31.java
@@ -12,24 +12,24 @@ public class Lesson31 extends Song
     switch (style) {
       case 1 :
         for (String name : names) {
-          singPart1(this::isStartWith, name, "Hip Hip Horray! For " + name);
+          singPart(this::isStartWith, name, "Hip Hip Horray! For " + name);
         }
         break;
       case 2 :
         for (String name : names){
-            singPart1(this::isContains, name, name.toUpperCase() + "! Yay " + name + "!");
+            singPart(this::isContains, name, name.toUpperCase() + "! Yay " + name + "!");
         }
         break;
       case 3 :
         for (String name : names) {
-        	singPart1(this::isFalse, name, null);
+        	singPart(this::isFalse, name, null);
         }
         break;
     }
   }
 
 
-private void singPart1(Function<String , Boolean> fn ,String name, String message) {
+private void singPart(Function<String , Boolean> fn ,String name, String message) {
 	if (fn.apply(name)) {
 	    sing(message);
 	  } else {
