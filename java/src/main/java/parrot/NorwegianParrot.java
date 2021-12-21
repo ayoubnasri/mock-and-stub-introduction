da6b77e2c752038a06e47a6e741790d601beaaf1
habiba <habiba.kessraoui@sofrecom.com>
2021-12-21 10:42:07 +0100

    Working...

--- java/src/main/java/parrot/NorwegianParrot.java
+++ java/src/main/java/parrot/NorwegianParrot.java
@@ -2,8 +2,11 @@ package parrot;
 
 public class NorwegianParrot extends Parrot{
 
+    private boolean isNailed;
+    
 	public NorwegianParrot(double voltage, boolean isNailed) {
-		super(null, voltage, isNailed);
+		super(null, voltage);
+		this.isNailed = isNailed;
 	}
 	public double getSpeed() {
         return (isNailed) ? 0 : getBaseSpeed(voltage);
