6c0d8b007e6b251da668ca747b82523c7bf31450
habiba <habiba.kessraoui@sofrecom.com>
2021-12-21 10:19:50 +0100

    Working...

--- java/src/main/java/parrot/EuropeanParrot.java
+++ java/src/main/java/parrot/EuropeanParrot.java
@@ -2,6 +2,10 @@ package parrot;
 
 public class EuropeanParrot extends Parrot{
 
+	public EuropeanParrot(ParrotTypeEnum type, int numberOfCoconuts, double voltage, boolean isNailed) {
+		super(type, numberOfCoconuts, voltage, isNailed);
+	}
+
 
 
 }
