94851905c64751154511cdbee5130841c1af0e53
Ayoub Nasri <ayoub1.nasri@sofrecom.com>
2021-12-21 14:41:38 +0100

    Working...

--- java/src/main/java/duplicationkata/Lesson1Straight.java
+++ java/src/main/java/duplicationkata/Lesson1Straight.java
@@ -7,15 +7,19 @@ public class Lesson1Straight extends Song
 {
 	
 	List<String> listText = new ArrayList();
+	
   public void singCatSong()
   {
     sing("We gave the cat to a little kid");
+    
     sing("But the cat came back");
     sing("The very next day");
     sing("Oh the cat came back");
     sing("We thought he was a goner");
     sing("But the cat came back, he just wouldn't go away");
+    
     sing("We sent the cat out on a boat");
+    
     sing("But the cat came back");
     sing("The very next day");
     sing("Oh the cat came back");
