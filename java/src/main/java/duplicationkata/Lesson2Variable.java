13d609861726caa97c50fccd981d837afb4e2ede
Ayoub Nasri <ayoub1.nasri@sofrecom.com>
2021-12-21 15:03:06 +0100

    Working...

--- java/src/main/java/duplicationkata/Lesson2Variable.java
+++ java/src/main/java/duplicationkata/Lesson2Variable.java
@@ -11,9 +11,9 @@ public class Lesson2Variable extends Song
 	  }
 
 	private void singRefrain(int i1) {
-		sing(i1 + " bottles of beer on the wall");
+		sing(i1 + refrain);
 	    sing(i1 + " bottles of beer");
 	    sing("Take one down, pass it around");  
-	    sing((i1 -1) +" bottles of beer on the wall");
+	    sing((i1 -1) + refrain);
 	}
 }
